323701b4ab7c54c3d73cc374c71508a637776e78
beta-proxy-8036 <mariacristina.simoes@gmail.com>
2018-10-23 18:45:10 +0000

    Done.

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,10 +1,10 @@
 describe "Not having any errors and being all green" do
 
   context 'NameError' do
-    it 'raises a NameError when encountering undefined barewords' do
+    it 'raises a NameError when encountering undefined barewords'
       expect{
         load './lib/a_name_error.rb'
-      }.to_not raise_error
+      }.to raise_error(NameError)
     end
   end
 
@@ -12,7 +12,7 @@ describe "Not having any errors and being all green" do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
-      }.to_not raise_error
+      }.to raise_error(SyntaxError)
     end
   end
 
